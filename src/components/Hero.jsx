7b65523649dab93478b5032e00721cbd2ecb09b4
bsc-com-24-23 <bsc-com-24-23@unima.ac.mw>
2025-12-31 21:25:41 +0200

    Fix hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import Typewriter from "typewriter-effect";
 const Hero = () => {
   return (
     <section className="relative w-full min-h-screen mx-auto">
-      <div className={`absolute inset-0 top-[80px] sm:top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+      <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
